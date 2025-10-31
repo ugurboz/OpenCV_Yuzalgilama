327a14674746b9dd53841cd4c509900f22633deb
ugurboz <uboz4707@hotmail.com>
2025-10-31 23:47:56 +0300

    Update

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+
+namespace OpenCV_Yuzalgilama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private VideoCapture _capture;
+        private CascadeClassifier _cascadeClassifier;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Yüz algılama için cascade dosyasını yükleyin
+            // Dosyanın adını doğru yazdığınızdan emin olun
+            _cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            // 0 varsayılan kameradır. Farklı bir kamera için 1, 2 vb. deneyebilirsiniz.
+            _capture = new VideoCapture(0);
+
+            // Her kare yakalandığında 'ProcessFrame' metodunu çalıştır.
+            Application.Idle += ProcessFrame;
+
+            button_start.Enabled = false; // Butonu devre dışı bırak.
+
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_capture == null || !_capture.IsOpened) return;
+
+            // Kameradan bir kare yakala (Bu, Mat tipinde bir nesnedir)
+            using (var matFrame = _capture.QueryFrame())
+            {
+                if (matFrame == null) return;
+
+                // *** YENİ: Mat nesnesini Image<Bgr, byte> tipine dönüştürerek çizim yapılabilir hale getiriyoruz. ***
+                using (var imageFrame = matFrame.ToImage<Bgr, byte>())
+                {
+                    if (imageFrame == null) return;
+
+                    // Görüntüyü gri tonlamaya dönüştür (algılama için)
+                    var grayFrame = imageFrame.Convert<Gray, byte>();
+
+                    // Yüzleri algıla
+                    Rectangle[] faces = _cascadeClassifier.DetectMultiScale(
+                        grayFrame,
+                        1.1,     // Scale Factor
+                        10,      // Minimum Komşular
+                        new Size(20, 20) // Minimum Yüz Boyutu
+                    );
+
+                    // Algılanan her yüz için çerçeve çiz (Artık imageFrame.Draw metodunu kullanabiliriz)
+                    foreach (var face in faces)
+                    {
+                        // Çizimi Image<TColor, TDepth> nesnesi üzerinde yapıyoruz
+                        imageFrame.Draw(face, new Bgr(Color.Red), 3); // Kırmızı çerçeve çiz
+                    }
+
+                    // İşlenmiş görüntüyü PictureBox'ta göster
+                    // Image<TColor, TDepth> nesnesini Bitmap'e dönüştürerek gösteriyoruz
+                    camera.Image = imageFrame.ToBitmap();
+                }
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Idle -= ProcessFrame; // Olayı durdur
+            if (_capture != null)
+            {
+                _capture.Dispose(); // Kaynakları serbest bırak
+            }
+            if (_cascadeClassifier != null)
+            {
+                _cascadeClassifier.Dispose();
+            }
+        }
+
+        private void camera_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ packages/Emgu.CV.runtime.mini.macos.4.12.0.5764/docs/README.md
@@ -0,0 +1,19 @@
+## About this package
+
+This package is part of the 4.12.0 release of Emgu CV. 
+
+This is the mini release that only contains the minimum amount of Open CV modules. For full release that contains all Open CV modules, use a release that is not labeled 'mini'.
+
+For online documentation please visit
+<https://www.emgu.com/wiki/files/4.12.0/document/index.html>
+
+Release note can be found at
+<https://www.emgu.com/wiki/index.php/Version_History#Emgu.CV-4.12.0>
+
+## Note
+
+If you are targeting .Net Framework, please read this first:
+<https://www.emgu.com/wiki/index.php/Download_And_Installation#Targeting_.Net_Framework>
+
+If you have issues getting the pacakge installed for your project, and your project use packages.config to manage nuget packages, the following instructions will help:
+<https://www.emgu.com/wiki/index.php/Download_And_Installation#Upgrade_from_packages.config_to_package_reference>
